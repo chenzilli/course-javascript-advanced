99cd404dc1876db423142dbc180d39a5c0e82808
chenzili <646582284@qq.com>
2018-03-22 08:02:47 +0800

    1

--- 课程练习/LS06/js/demo14.js
+++ 课程练习/LS06/js/demo14.js
@@ -3,11 +3,19 @@
  */
 //逻辑与、或的基本理解
 console.log(2>1&&4<5);
+//答：ture
 console.log(true&&(!2));
+//答：false
 console.log(false&&("2" == 2));
+//答：false
 console.log(false&&false);
+//答：false
 
 console.log(2>1||4<5);
+//答：true
 console.log(true||(!2));
+//答：true
 console.log(false||("2" == 2));
+//答：true
 console.log(false||false);
+//答：false0000
